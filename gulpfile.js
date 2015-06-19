95fa110b1c59d03f8813b217cf833fb0f90a3d9b
Alastair Paragas <alastairparagas@gmail.com>
2015-06-19 10:54:50 -0400

    Gulp-Watch for watching files

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,22 @@
 		plumber = require('gulp-plumber'),
 		sourcemaps = require('gulp-sourcemaps'),
 		concat = require('gulp-concat'),
-		uglify = require('gulp-uglify');
+		uglify = require('gulp-uglify'),
+		watch = require('gulp-watch');
 		
 	function scssCompile() {
 		var scssSettings = {
 				outputStyle: 'compressed',
 				errLogToConsole: true
+			},
+			plumberSettings = {
+				errorHandler: function (error) {
+					console.log("SASS Error: " + error);	
+				}
 			};
 		
 		return gulp.src('./styles/scss/main.scss')
+			.pipe(plumber(plumberSettings))
 			.pipe(sourcemaps.init())
 			.pipe(sass(scssSettings))
 			.pipe(sourcemaps.write())
@@ -50,8 +57,8 @@
 	gulp.task('jsMinify', jsMinify);
 	
 	gulp.task('dev', function () {
-		gulp.watch('./styles/scss/**/*.scss', ['scssCompile']);
-		gulp.watch(['./app/**/*.js'], ['jsMinify']);
+		watch('./styles/scss/**/*.scss', scssCompile);
+		watch('./app/**/*.js', jsMinify);
 	});
 	
 	gulp.task('default', ['dev']);
